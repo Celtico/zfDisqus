63fa16766723f2e53e68cf77d2c6cca296ee820a
Max Tsepkov <max@garygolden.me>
2013-05-05 12:16:16 +0600

    autoloader config

--- Module.php
+++ Module.php
@@ -2,7 +2,21 @@
 
 namespace YogiDisqus;
 
-class Module
-{
+use Zend\ModuleManager;
 
+class Module implements ModuleManager\Feature\AutoloaderProviderInterface
+{
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
 }
